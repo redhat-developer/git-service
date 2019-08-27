55d63bc22c696c9acefadf11be5ee8807989d139
Pratik Jagrut <pjagrut@redhat.com>
2019-08-27 17:41:18 +0530

    [WIP]Add api to get exposed container port from Dockerfile for Gitlab.

--- src/client_test.ts
+++ src/client_test.ts
@@ -1,13 +1,21 @@
 import {GitSource, SecretType} from "./service/modal/gitsource";
-import {BitbucketService} from "./service/bitbucket_service";
+import {GitlabService} from "./service/gitlab_service";
 
 const gr = new GitSource(
-  "https://bitbucket.org/akshinde/testgitsource",
+  "https://gitlab.com/jpratik999/devconsole-operator.git",
   SecretType.NO_AUTH,
-  null
+  ''
 );
 
-const gs = new BitbucketService(gr);
+const gs = new GitlabService(gr);
+
+//Expected output: true
+gs.isDockerfilePresent()
+  .then(resp => console.log(resp))
+  .catch(err => console.error(err.message));
+
+//Expected output: Dockerfile content
 gs.getDockerfileContent()
   .then(resp => console.log(resp))
   .catch(err => console.error(err.message));
+
